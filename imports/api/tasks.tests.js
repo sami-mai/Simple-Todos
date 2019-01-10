c0c49e72d22841c97128d96b6f1a04046d2228e9
EIT-sami-mai <samirah.maison@meltwater.org>
2019-01-10 03:10:54 +0000

    add test for 'tasks.insert' method

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -17,8 +17,23 @@ import {
 if (Meteor.isServer) {
   describe('Tasks', () => {
     describe('methods', () => {
-      const userId = Random.id();
+      const username = 'sami-mai';
       let taskId;
+      let userId;
+
+      before(() => {
+        let user = Meteor.users.findOne({username: username});
+        if (!user) {
+          userId = Accounts.createUser({
+            'username': username,
+            'email': 'sam@mai.com',
+            'password': '0101010',
+          }); 
+        } else {
+          userId = user._id;
+        }
+
+      });
 
       beforeEach(() => {
         Tasks.remove({});
@@ -29,6 +44,8 @@ if (Meteor.isServer) {
           username: 'tmeasday',
         });
       });
+
+      // test 'tast.remove' method
       it('can delete owned task', () => {
         // Find the internal implementation of the task method so we can
         // test it in isolation
@@ -45,6 +62,20 @@ if (Meteor.isServer) {
         // Verify that the method does what we expected
         assert.equal(Tasks.find().count(), 0);
       });
+
+      //  test 'tasks.insert' method
+      it('can insert task', () => {
+        let text = 'Insert new task';
+        const insertTask = Meteor.server.method_handlers['tasks.insert'];
+        // caution Error: Meteor.userId can only be invoked in method calls or publications.
+        const invocation = {
+          userId
+        };
+        insertTask.apply(invocation, [text]);
+        assert.equal(Tasks.find().count(), 2);
+      });
+
+
     });
   });
 }
